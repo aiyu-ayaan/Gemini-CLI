d1f08eeacfa244c5f4ad78929744471df345b02e
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-06-09 17:01:28 +0530

    Markdown support is added !!

--- gemini-cli.py
+++ gemini-cli.py
@@ -1,5 +1,20 @@
 import gemini as gemini
+import argparse
+import sys
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Gemini CLI')
+    parser.add_argument('question', type=str, help='Question to ask Gemini')
+    # add optional arguments here"
+    parser.add_argument('--word-limit', '-wl', help='Word limit for the response', type=int, default=0)
+    # parse the arguments
+    args = parser.parse_args()
+    question = args.question.strip()
+    max_words = args.word_limit
+    # check if the question is empty
+    if len(question) == 0:
+        print('Question cannot be empty')
+        sys.exit(1)
+    # create a Gemini instance
     g = gemini.Gemini()
-    print(g.ask('What is the meaning of life? in 30 words or less.'))
+    g.ask(question, max_words)
--- gemini/gemini.py
+++ gemini/gemini.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from rich.console import Console
+from rich.markdown import Markdown
 
 load_dotenv(override=True)
+console = Console()
 
 
 class Gemini:
@@ -11,6 +14,20 @@ class Gemini:
         genai.configure(api_key=google_api_key)
         self.__model = genai.GenerativeModel('gemini-1.5-flash')
 
-    def ask(self, question: str):
-        print('Thinking...')
-        return self.__model.generate_content(question).text
+    def ask(self, question: str, max_words: int = 0):
+        console.print(f'🐼', f'Asking: {question}\n', style='bold blue')
+        with console.status('Generating response...') as status:
+            try:
+                has_error = False
+                response = self.__model.generate_content(
+                    question if max_words == 0 else question + f'Word limit {max_words}').text
+
+            except Exception as e:
+                has_error = True
+                response = e
+
+        if has_error:
+            console.print('❌', f' Error: {response}', style='bold red')
+        else:
+            markdown = Markdown(response)
+            console.print(markdown, style='bold green')
